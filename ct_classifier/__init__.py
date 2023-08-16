0b26023b74075320f51fd78e443a5db0a02a1fa6
GrunCrow <albamrqz751@gmail.com>
2023-08-16 23:37:32 +0000

    preparing model folder yayyyy (it will be yolov8)

---
+++ ct_classifier/__init__.py
@@ -0,0 +1,7 @@
+'''
+    GrunCrow
+'''
+
+# Info: these "__init__.py" files go into every folder and subfolder that
+# contains Python code. It is required for Python to find all the scripts you
+# created and for you to be able to import them.
